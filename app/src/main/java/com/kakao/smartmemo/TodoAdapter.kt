f0be4fbe73b7ffdb409193309e505604a1c3bdf4
hyo-jin-jeong <gaya309@naver.com>
2020-04-02 22:59:51 +0900

    changed about todolist

--- app/src/main/java/com/kakao/smartmemo/TodoAdapter.kt
+++ app/src/main/java/com/kakao/smartmemo/TodoAdapter.kt
@@ -16,10 +16,9 @@ class TodoAdapter : RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
     private var datas: MutableList<TodoDTO> =
         mutableListOf(TodoDTO("약먹기"), TodoDTO("도서관 책 반납"))
 
-    private lateinit var textviewCancel : TextView
-    private lateinit var bellTodo : Button
-    private lateinit var plusTodo : ImageButton
-    private lateinit var editTextTodo: EditText
+    lateinit var textview : TextView
+    lateinit var bellTodo : Button
+    lateinit var checkBox : CheckBox
 
     //View Holder생성
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(parent)
@@ -29,30 +28,34 @@ class TodoAdapter : RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
 
     //View가 Bind되었을때의 설정
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         datas[position].let {
             with(holder) {
-                txt_todo.text = it.todoContent
+                textview=this.txt_todo
+             /*   bellTodo=this.btn_todo
+                checkBox= this.checkBox_todo*/
+                textview.text = it.todoContent
+
             }
         }
 
         holder.itemView.setOnClickListener(View.OnClickListener {
-
-            /*plusTodo = it.findViewById(R.id.btn_plus)
-            plusTodo.setOnClickListener(View.OnClickListener {
-                editTextTodo = it.editText_todo
-                datas.add(TodoDTO(editTextTodo.toString()))
-            })*/
-            Log.v("seyuuun", it.toString())
-            textviewCancel = it.findViewById(R.id.textView_todo) //취소선
+            textview = it.findViewById(R.id.textView_todo) //취소선
             bellTodo = it.findViewById(R.id.btn_todo)
+            checkBox = it.findViewById(R.id.checkBox_todo)
+            if(checkBox.isChecked){
+                textview.paintFlags =  Paint.STRIKE_THRU_TEXT_FLAG  //취소선 설정
+            }
+            else{
+                textview.paintFlags = 0  //취소선 설정
+            }
             bellTodo.setOnClickListener(View.OnClickListener {
                 val alarmSettingsIntent = Intent(it.context, TodoListActivity::class.java)
                 Log.v("seyuuuun", "Intent")
                 it.context.startActivity(alarmSettingsIntent)
                 Log.v("seyuuuun", "Intent2")
             })
-            textviewCancel.setPaintFlags(textviewCancel.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG)  //취소선 설정
-            Toast.makeText(it.context, "Clicked :${datas.get(position).todoContent}", Toast.LENGTH_LONG).show()
+
 
         })
     }
@@ -67,7 +70,8 @@ class TodoAdapter : RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
 
     inner class ViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.todo_list_item, parent, false)) {
         val txt_todo = itemView.textView_todo
-
+        val btn_todo = itemView.btn_todo
+        val checkBox_todo = itemView.checkBox_todo
 
 
     }
