8ff508390ca317183ae86efffa19863f2070c6de
NASEYUN <gksquf71@gmail.com>
2020-04-02 19:50:27 +0900

    ViewStub modified

--- app/src/main/java/com/kakao/smartmemo/TodoListActivity.kt
+++ app/src/main/java/com/kakao/smartmemo/TodoListActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.*
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.CompoundButton
 import android.widget.LinearLayout
 import android.widget.Switch
 import android.widget.Toolbar
@@ -17,22 +18,38 @@ import kotlinx.android.synthetic.main.time_location_settings.*
 import java.util.zip.Inflater
 
 class TodoListActivity : AppCompatActivity() {
-    private lateinit var alarmswitch : Switch
-    private lateinit var alarmlayout : LinearLayout
+    private lateinit var alarmswitch_time : Switch
+    private lateinit var alarmswitch_location : Switch
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.time_location_settings)
 
-        val todostub = stub_alarm
-        val todoview = todostub.inflate()
+        val todostub_time = stub_alarm_time
+        val todostub_location = stub_alarm_location
 
-        alarmswitch = this.findViewById(R.id.switch_time)
+        alarmswitch_time = this.findViewById(R.id.switch_time)
+        alarmswitch_location = this.findViewById(R.id.switch_location)
 
-        if(alarmswitch.isChecked) {
-            todoview.visibility = VISIBLE
-        }else {
-            todoview.visibility = GONE
+        alarmswitch_time.setOnCheckedChangeListener { compoundButton, isChecked->
+            if(isChecked) {
+                todostub_time.visibility = VISIBLE
+                Log.v("seyuuuun", "viewstub")
+            }else {
+                todostub_time.visibility = GONE
+                Log.v("seyuuuun", "viewstub_gone")
+            }
         }
+
+        alarmswitch_location.setOnCheckedChangeListener { compoundButton, isChecked->
+            if(isChecked) {
+                todostub_location.visibility = VISIBLE
+                Log.v("seyuuuun", "viewstub")
+            }else {
+                todostub_location.visibility = GONE
+                Log.v("seyuuuun", "viewstub_gone")
+            }
+        }
+
     }
 }
