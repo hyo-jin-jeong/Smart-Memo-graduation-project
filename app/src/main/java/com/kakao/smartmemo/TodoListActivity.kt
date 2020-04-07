2161d0f290a1032b2964a0e35425c9d103d334f7
hyo-jin-jeong <gaya309@naver.com>
2020-04-07 23:25:34 +0900

    todolist alarm chaned

--- app/src/main/java/com/kakao/smartmemo/TodoListActivity.kt
+++ app/src/main/java/com/kakao/smartmemo/TodoListActivity.kt
@@ -14,13 +14,15 @@ import kotlinx.android.synthetic.main.alarm_settings_time.*
 import kotlinx.android.synthetic.main.time_location_settings.*
 
 class TodoListActivity : AppCompatActivity() {
+
     private lateinit var myToolbar: Toolbar
     private lateinit var alarmswitch_time : Switch
     private lateinit var alarmswitch_location : Switch
     private lateinit var timebtn: ImageButton
+    private lateinit var timeagainbtn : ImageButton
     private lateinit var placebtn: ImageButton
     private lateinit var placelistview : ListView
-
+    private lateinit var placeagainbtn: ImageButton
     private var placeList = arrayListOf<PlaceDTO>(PlaceDTO("연세병원"))
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,6 +50,7 @@ class TodoListActivity : AppCompatActivity() {
         timebtn = view_time.findViewById(R.id.btn_time_settings)
         placebtn = view_location.findViewById(R.id.btn_place_choice)
         placelistview = view_location.findViewById(R.id.listview_place)
+        placeagainbtn = view_location.findViewById(R.id.btn_repeat_place)
 
         alarmswitch_time.setOnCheckedChangeListener { compoundButton, isChecked->
             if(isChecked) {
@@ -82,7 +85,6 @@ class TodoListActivity : AppCompatActivity() {
                     dialog.show()
                 }
 
-                Log.v("seyuuuun", "viewstub")
             }else {
                 todostub_time.visibility = GONE
                 Log.v("seyuuuun", "viewstub_gone")
@@ -97,10 +99,12 @@ class TodoListActivity : AppCompatActivity() {
                     this.startActivity(placechoiceIntent)
                 })
                 Log.v("seyuuuun", "viewstub")
+
             }else {
                 todostub_location.visibility = GONE
                 Log.v("seyuuuun", "viewstub_gone")
             }
+
         }
 
         placelistview.adapter = PlaceListAdapter(this, placeList)
