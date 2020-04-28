4d897f86b9030a01a1eb6997fe7612dd8168710d
hyo-jin-jeong <gaya309@naver.com>
2020-04-28 20:55:50 +0900

    Group create chnage

--- app/src/main/java/com/kakao/smartmemo/Model/GroupModel.kt
+++ app/src/main/java/com/kakao/smartmemo/Model/GroupModel.kt
@@ -5,13 +5,15 @@ import com.kakao.smartmemo.Object.UserObject
 
 class GroupModel {
     private var firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
-    var groupAddToUser = firestore.collection("User").document(UserObject.email).collection("GroupId")
-    var groupAddToGroup = firestore.collection("Group")
+    private var groupAddToUser = firestore.collection("User").document("${UserObject.email}").collection("GroupId")
+    private var groupAddToGroup = firestore.collection("Group")
+
     fun addGroup(groupName: String, color: Int) {
         //그룹 관련된 DB작업
-        groupAddToUser.document(groupName).set(hashMapOf("group_id" to UserObject.email + groupName,"group_color" to color))
+        groupAddToUser.document("$groupName").set(hashMapOf("group_id" to UserObject.email + groupName,"group_color" to color))
         groupAddToGroup.document(UserObject.email + groupName).set(hashMapOf("group_name" to groupName,"group_color" to color))
-        groupAddToGroup.document(UserObject.email + groupName).collection("Member").document(UserObject.email)
+        groupAddToGroup.document(UserObject.email + groupName).collection("Member").document("MemberId").set(
+            hashMapOf("email" to UserObject.email))
     }
     fun updateGroup() {
 
